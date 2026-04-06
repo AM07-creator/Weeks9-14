e86d55c0cc563a52df8dd18c983fda75dfeaf62b
AM07-creator <mancaa@sheridancollege.ca>
2026-04-05 22:53:23 -0400

    prefaab spawns when stopwatch hits 3 seconds, appearing at random x positions on screen

--- Assets/Scripts/DebrisSpawner.cs
+++ Assets/Scripts/DebrisSpawner.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class DebrisSpawner : MonoBehaviour
 {
+    //Set the Debris GameObject, as well as speed variable for stopwatch. Range limit for where the Debris can spawn on x pos
+
+    public float spawnTime = 0;
+    public GameObject debrisPrefab;
+    float minX = -8;
+    float maxX = 8;
+    float fixedY = 6f;
+    float fixedZ = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,18 @@ public class DebrisSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Creates a stopwatch that counts up to 3 seconds, then causes a debris to instantiate and restart the stopwatch at 0
+        spawnTime += Time.deltaTime;
+        if (spawnTime > 3)
+        {
+            spawnTime = 0;
+			//Spawn the debris at a randomly chosen x pos. No rotating the prefab
+			float randomX = Random.Range(minX, maxX);
+            Vector3 spawnPosition = new Vector3(randomX, fixedY, fixedZ);
+			Instantiate(debrisPrefab, spawnPosition, Quaternion.identity);
+		}
+        //Destroy debris prefab at y = -6 or somewhere below the screen
+
+        //
     }
 }
